201c512ae6bf951d00979f9652800eb3fc0e8e03
ThangHD082002 <thanghd2708@EMAIL.com>
2024-07-08 10:06:27 -0700

    Chapter 7 - Signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,14 @@ Rails.application.routes.draw do
   get 'demo_partials/edit'
   get 'static_pages/home'
   get 'static_pages/help'
+  root "static_pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users, only: :show
+
 end
